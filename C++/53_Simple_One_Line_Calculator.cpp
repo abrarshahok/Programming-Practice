699d70d6bcb2a0995290e9695d356675ffe9228b
Abrar Ahmed <abrarahmedshahok12@gmail.com>
2023-01-14 20:02:52 +0500

    Modified Files

--- C++/53_Simple_One_Line_Calculator.cpp
+++ C++/53_Simple_One_Line_Calculator.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     string str;
-    int temp;
-    int ans=0;
+    long long temp;
+    long long ans=0;
     char op ='+';
     cout<<"Enter Equation : ";
     getline(cin,str);
@@ -17,6 +17,6 @@ int main(){
         else{continue;}
         ss>>op;
     }
-    cout<<"Calculation    : "<<ans<<endl;
+    cout<<"Answer         : "<<ans<<endl;
 }
 
